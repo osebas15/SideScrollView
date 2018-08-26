cc11b4babb4776e63e7977c309725fbe5ff43ee0
Sebastian Aguirre <osebas15@gmail.com>
2018-08-25 23:03:53 -0400

    Initial Commit

---
+++ SideScrollView/SideScrollView.h
@@ -0,0 +1,19 @@
+//
+//  SideScrollView.h
+//  SideScrollView
+//
+//  Created by Sebastian Aguirre on 8/25/18.
+//  Copyright © 2018 SeaCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideScrollView.
+FOUNDATION_EXPORT double SideScrollViewVersionNumber;
+
+//! Project version string for SideScrollView.
+FOUNDATION_EXPORT const unsigned char SideScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideScrollView/PublicHeader.h>
+
+
